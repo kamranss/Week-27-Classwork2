224da4da55ccf3fdce6d59e6bdc073496de55644
kamranss <kamranss@code.edu.az>
2023-03-03 15:35:41 +0400

    Refactor: task updated

--- assets/script/gradient.js
+++ assets/script/gradient.js
@@ -1,6 +1,6 @@
+
+// Generate Gradient
 let GeneratedGradient = document.querySelector("h3");
-// let color1 = document.querySelector(".color1");
-// let color2 = document.querySelector(".color2");
 
 var color1 = document.getElementById("color1");
 var color2 = document.getElementById("color2");
@@ -15,21 +15,3 @@ let Body = document.getElementById("Body")
 color1.addEventListener("input",setGradient);
 
 color2.addEventListener("input", setGradient);
-
-
-
-
-// function setGradient() {
-// 	Body.style.background = 
-// 	"linear-gradient(to right, " 
-// 	+ color1.value 
-// 	+ "," 
-// 	+ color2.value 
-// 	+ ")";
-
-// 	GeneratedGradient.textContent = Body.style.background + ";";
-// }
-
-// color1.addEventListener("input", setGradient);
-
-// color2.addEventListener("input", setGradient);
